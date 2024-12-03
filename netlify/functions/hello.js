1ba2f5b8fa6dc3ea92ab9950a73b0b10d146b9ed
Shyama5676 <shyama5676@gmail.com>
2024-12-02 21:33:25 -0500

    Update hello.js
    ....

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -10,6 +10,6 @@ exports.handler = async event => {
     const subject = event.queryStringParameters.name || 'World'
     return {
         statusCode: 200,
-        body: `>>Hello World ${subject}!`,
+        body: `>>Hello World.... ${subject}!`,
     }
 }
